70dc06c25c062425a1c29178a9b8b8db3419f0f0
OrangePi <orangpi@8lanes.co>
2024-03-27 22:41:17 +0700

    feat: 🎸 get deviceId from pgrok.yml

--- src/commons/helpers/utils.helper.ts
+++ src/commons/helpers/utils.helper.ts
@@ -1,3 +1,17 @@
+import 'dotenv/config';
+
+import { readFileSync } from 'fs';
+import { load } from 'js-yaml';
+
+interface PgrokConfig {
+  remote_addr: string;
+  forward_addr: string;
+  token: string;
+}
+
+const pgrokConfigFile = process.env.PGROK_CONFIG || "~/.config/pgrok/pgork.yml"
+const doc = load(readFileSync(pgrokConfigFile, "utf8")) as PgrokConfig;
+
 export const getDeviceId = (): string => {
-  return '12345';
+  return doc.token;
 };
