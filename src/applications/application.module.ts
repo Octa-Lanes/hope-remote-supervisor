6aa10034bd8958a79e4dcea2e76236bb6f60dae5
OrangePi <orangpi@8lanes.co>
2024-04-19 16:03:06 +0700

    fix: 🐛 not found error

--- src/applications/application.module.ts
+++ src/applications/application.module.ts
@@ -1,12 +1,11 @@
 import { Global, Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { LogRunner } from 'src/applications/schedules/runner/logRunner.schedule';
 import exportedUseCases from 'src/applications/usecases';
 
 @Global()
 @Module({
   imports: [ScheduleModule.forRoot()],
-  providers: [...exportedUseCases, LogRunner],
+  providers: [...exportedUseCases],
   exports: [...exportedUseCases],
 })
 export class ApplicationModule {}
