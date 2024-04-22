f798e300ebfb47a405a39febcc39dc4a51f695e8
melb.jeds <jedezese04@gmail.com>
2024-04-22 13:51:49 +0700

    fix: 🐛 register runner service

--- src/applications/application.module.ts
+++ src/applications/application.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { LogRunner } from 'src/applications/schedules/runner/logRunner.schedule';
 import exportedUseCases from 'src/applications/usecases';
 
 @Global()
 @Module({
   imports: [ScheduleModule.forRoot()],
-  providers: [...exportedUseCases],
+  providers: [...exportedUseCases, LogRunner],
   exports: [...exportedUseCases],
 })
 export class ApplicationModule {}
