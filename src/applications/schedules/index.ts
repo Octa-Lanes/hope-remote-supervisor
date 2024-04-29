5feb463f4d6bba84fb23a69aa2b20b5e3d2c7bac
melb.jeds <jedezese04@gmail.com>
2024-04-29 14:46:31 +0700

    fix: 🐛 debug file stream

--- src/applications/schedules/index.ts
+++ src/applications/schedules/index.ts
@@ -1,4 +1,4 @@
 import { LogRunner } from 'src/applications/schedules/runner/logRunner.schedule';
 import { PulseRunner } from 'src/applications/schedules/runner/pulseRunner.schedule';
 
-export const exportedRunners = [PulseRunner, LogRunner];
+export const exportedRunners = [PulseRunner];
